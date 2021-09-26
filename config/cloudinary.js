933a53ebde66328430652aba19968a82ec92ed12
Sreejan <sreejan147@gmail.com>
2021-09-26 11:30:39 +0530

    feat: add cloudinary config file

---
+++ config/cloudinary.js
@@ -0,0 +1,14 @@
+const cloudinary = require("cloudinary");
+// require("dotenv").config({ path: "../.env" });
+require("dotenv").config();
+
+console.log(process.env.MONGO_URI);
+console.log(process.env.CLOUDINARY_API_KEY);
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+module.exports = { cloudinary };

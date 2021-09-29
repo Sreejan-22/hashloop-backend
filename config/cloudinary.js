1179e893ad7a51fd9f0de69acbac6e377bb9e54a
Sreejan <sreejan147@gmail.com>
2021-09-29 14:50:19 +0530

    fix: fixed an issue

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,9 +1,5 @@
+require("dotenv").config({ path: "../.env" });
 const cloudinary = require("cloudinary").v2;
-// require("dotenv").config({ path: "../.env" });
-require("dotenv").config();
-
-console.log(process.env.MONGO_URI);
-console.log(process.env.CLOUDINARY_API_KEY);
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,

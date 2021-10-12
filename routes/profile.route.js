129db545acde0677405f540d52ee9bc899220170
Sreejan <sreejan147@gmail.com>
2021-10-12 17:53:06 +0530

    feat: updated profile route

--- routes/profile.route.js
+++ routes/profile.route.js
@@ -9,12 +9,12 @@ const { checkAuthentication } = require("../middlewares/auth.middleware");
 const router = Router();
 
 // get profile of a user
-router.get("/profile/:id", checkAuthentication, getProfileOfUser);
+router.get("/profile/:id", getProfileOfUser);
 
 // create a profile for a new user
 // initially after sign up, the username, email, name fields will get assigned their respective values
 // later on the profile will be updated
-router.post("/profile", createProfile);
+router.post("/profile", checkAuthentication, createProfile);
 
 // edit profile
 router.put("/profile/:id", checkAuthentication, editProfile);
